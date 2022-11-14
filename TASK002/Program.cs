a6ec9a09f58a75d2776a0858a996fbca10a84c31
AlexandrGolub <aholub1979@gmail.com>
2022-11-14 15:28:24 +0300

    Add HomeWork

---
+++ TASK002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Напишите первое целое число ");
+int a = Convert.ToInt32 (Console.ReadLine()!);
+Console.WriteLine($"Число a равно {a}");
+Console.WriteLine("Напишите второе целое число ");
+int b = Convert.ToInt32 (Console.ReadLine()!);
+Console.WriteLine($"Число b равно {b}");
+
+int max = 0;
+if (a < b) max = b;
+else if (a > b) max = a;
+Console.WriteLine($"max={max}");
+
+
+
---
+++ TASK004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Напишите первое целое число ");
+int a = Convert.ToInt32 (Console.ReadLine()!);
+Console.WriteLine($"Число a равно {a}");
+Console.WriteLine("Напишите второе целое число ");
+int b = Convert.ToInt32 (Console.ReadLine()!);
+Console.WriteLine($"Число b равно {b}");
+Console.WriteLine("Напишите третье целое число ");
+int c = Convert.ToInt32 (Console.ReadLine()!);
+Console.WriteLine($"Число c равно {c}");
+
+int max = a;
+if (b > a ) max = b;
+if (c > b) max = c;
+Console.WriteLine($"max={max}");
